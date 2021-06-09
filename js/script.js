e430b96b2f58a9a39804b2825d49036c6683e870
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2021-06-09 12:49:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    /* header fixed */
+
+    checkScroll(scrollOffset);    
+
+    $(window).on("scroll", function() {
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+        
+    });
+
+    function checkScroll(scrollOffset) {
+
+         if(scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /* Smoth scroll */
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+
+    var $this = $(this),
+        blockId = $this.data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate ({
+            scrollTop: blockOffset
+        }, 500);
+
+    });
+
+    /* Menu nav toggle*/
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $("#nav").toggleClass("active");
+
+    });
+
+
+  
+
+
+
+
+});
