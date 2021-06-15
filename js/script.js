ee98bd63cacf314fbdb81e7bacec56142abefbd3
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2021-06-15 15:54:42 +0300

    pravki

--- js/script.js
+++ js/script.js
@@ -31,14 +31,14 @@ $(function() {
         event.preventDefault();
 
 
-    var $this = $(this),
-        blockId = $this.data('scroll'),
-        blockOffset = $(blockId).offset().top;
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
 
         $("#nav a").removeClass("active");
         $this.addClass("active");
 
-        $("html, body").animate ({
+        $("html, body").animate({
             scrollTop: blockOffset
         }, 500);
 
@@ -48,9 +48,23 @@ $(function() {
     $("#nav_toggle").on("click", function(event) {
         event.preventDefault();
 
+
+        $(this).toggleClass("active");
         $("#nav").toggleClass("active");
 
     });
+
+    /* collapse*/
+    $("[data-collapse]").on("click", function(event){
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+
+    });
+   
 
 
   
